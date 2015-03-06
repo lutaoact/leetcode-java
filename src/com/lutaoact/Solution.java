26e837e734063941b3f73875cd376b49e56147ea
lutaoact <lutaoact@hotmail.com>
2015-03-06 17:52:33 +0800

    twoSum: refactor

--- src/com/lutaoact/Solution.java
+++ src/com/lutaoact/Solution.java
@@ -4,20 +4,18 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-    	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-    	int[] indexes = new int[2];
-    	
-    	for (int i = 0; i < numbers.length; i++) {
-    		map.put(numbers[i], i);
-    	}
-    	for (int i = 0; i < numbers.length; i++) {
-    		Integer index = map.get(target - numbers[i]);
-    		if (index != null && index != i) {
-    			indexes[0] = i + 1;
-    			indexes[1] = index + 1;
-    			return indexes;
-    		}
-    	}
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] indexes = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(numbers[i])) {
+                Integer index = map.get(numbers[i]);
+                indexes[0] = index + 1;
+                indexes[1] = i + 1;
+                return indexes;
+            } else {
+                map.put(target - numbers[i], i);
+            }
+        }
         return indexes;
     }
 }
