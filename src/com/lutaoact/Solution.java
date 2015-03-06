50422ee22d981355ad08f21bcefe62f43798b375
lutaoact <lutaoact@hotmail.com>
2015-03-06 18:02:19 +0800

    ListNode.java

--- src/com/lutaoact/Solution.java
+++ src/com/lutaoact/Solution.java
@@ -18,4 +18,8 @@ public class Solution {
         }
         return indexes;
     }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(l1.val + l2.val);
+        return result;
+    }
 }
