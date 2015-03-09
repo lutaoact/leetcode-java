d42721217d34c171e456dbe35918ab452c02c33d
lutaoact <lutaoact@hotmail.com>
2015-03-09 13:34:00 +0800

    addTwoNumbers

--- src/com/lutaoact/LeetCodeRun.java
+++ src/com/lutaoact/LeetCodeRun.java
@@ -4,11 +4,47 @@ import java.util.Arrays;
 
 public class LeetCodeRun {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Solution solution = new Solution();
-    	int[] a = {2, 7, 11, 15};
-		System.out.println(Arrays.toString(solution.twoSum(a, 8)));
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Solution solution = new Solution();
+//      int[] a = {2, 7, 11, 15};
+//      System.out.println(Arrays.toString(solution.twoSum(a, 8)));
 
+        /*
+           addTwoNumbers
+        */
+//        ListNode node1 = new ListNode(5);
+//        node1.next = new ListNode(4);
+//        node1.next.next = new ListNode(3);
+//        node1.next.next.next = new ListNode(9);
+//        node1.next.next.next.next = new ListNode(2);
+//
+//        ListNode node2 = new ListNode(5);
+//        node2.next = new ListNode(6);
+//        node2.next.next = new ListNode(9);
+//
+//        ListNode node = solution.addTwoNumbers(node1, node2);
+//
+//        ListNode current = node;
+//        while (current != null) {
+//            System.out.println(current.val);
+//            current = current.next;
+//        }
+        /*
+           addTwoNumbers __END__
+        */
+//      ListNode result = node;
+//      for (int i = 0; i < 3; i++) {
+//          node.next = new ListNode(i);
+//          node = node.next;
+//      }
+//      ListNode other = solution.copyList(result);
+//      ListNode other = result;
+//      ListNode current = other.next;
+//        System.out.println(current.val);
+//        while (current != null) {
+//            System.out.println(current.val);
+//            current = current.next;
+//        }
+    }
 }
--- src/com/lutaoact/Solution.java
+++ src/com/lutaoact/Solution.java
@@ -18,8 +18,50 @@ public class Solution {
         }
         return indexes;
     }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(l1.val + l2.val);
+        ListNode node = new ListNode((l1.val + l2.val) % 10);
+        ListNode result = node;
+
+        int carry = (l1.val + l2.val) / 10;
+        ListNode l1Current = l1.next;
+        ListNode l2Current = l2.next;
+        while (l1Current != null || l2Current != null) {
+            if (l1Current == null) {
+                node.next = new ListNode((l2Current.val + carry) % 10);
+                node = node.next;
+                carry = (l2Current.val + carry) / 10;
+                l2Current = l2Current.next;
+            } else if (l2Current == null) {
+                node.next = new ListNode((l1Current.val + carry) % 10);
+                node = node.next;
+                carry = (l1Current.val + carry) / 10;
+                l1Current = l1Current.next;
+            } else {
+                node.next = new ListNode((l1Current.val + l2Current.val + carry) % 10);
+                node = node.next;
+                carry = (l1Current.val + l2Current.val + carry) / 10;
+                l1Current = l1Current.next;
+                l2Current = l2Current.next;
+            }
+        }
+        if (carry > 0) {
+            node.next = new ListNode(carry);
+        }
+        return result;
+    }
+    public ListNode copyList(ListNode listNode) {
+        if (listNode == null) return null;
+
+        ListNode node = new ListNode(listNode.val);
+        ListNode result = node;
+
+        ListNode current = listNode.next;
+        while (current != null) {
+            node.next = new ListNode(current.val);
+            node = node.next;
+            current = current.next;
+        }
         return result;
     }
 }
