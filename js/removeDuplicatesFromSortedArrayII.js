db00daf3ace146f8b117d56291f964c4afa11228
lutaoact <lutaoact@hotmail.com>
2015-08-27 07:38:30 +0800

    removeDuplicatesFromSortedArrayII.js

---
+++ js/removeDuplicatesFromSortedArrayII.js
@@ -0,0 +1,17 @@
+function removeDuplicates(nums) {
+  var length = nums.length;
+  if (length === 0) return 0;
+  var count = 1, index = 0;
+  for (var i = 1; i < length; i++) {
+    if (nums[index] === nums[i] && ++count <= 2) {
+      nums[++index] = nums[i++];
+    }
+    if (nums[index] !== nums[i] && i < length) {
+      nums[++index] = nums[i];
+      count = 1;
+    }
+  }
+  return index + 1;
+}
+console.log(removeDuplicates([1, 1, 1, 1, 2, 2, 2, 2, 3, 4, 5]));
+console.log(removeDuplicates([1, 1]));
